c2b60f5655355c94249e5421c344fef6451bd68b
yoheeeei <yohei.contact@gmail.com>
2023-09-15 09:34:28 +0900

    feat: :sparkles: add chromatic

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,5 @@
 import { ButtonProps } from "./type";
 
 export const Button = ({ children }: ButtonProps) => {
-  return <button className="bg-gray-600 px-8 py-2 rounded-full text-white font-bold hover:bg-gray-500">{children}</button>;
+  return <button className="bg-gray-600 px-10 py-2 rounded-full text-white font-bold hover:bg-gray-500">{children}</button>;
 };
